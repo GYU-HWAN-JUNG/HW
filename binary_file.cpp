0f9b76a4b929cc9f6db997c964965d20c6c9c8ca
GYU-HWAN-JUNG <127587101+GYU-HWAN-JUNG@users.noreply.github.com>
2023-04-06 19:52:37 +0900

    Update binary_file.cpp

--- binary_file.cpp
+++ binary_file.cpp
@@ -7,41 +7,47 @@ const float pi = 3.141592;
 int main() {
     int n = 200;
     float f = 1000;
-    short a = 30, p = 10;
-    float* samples, *samples2, *rr, *RR;
-    rr = new float[n];
-    RR = new float[n];
-    samples = new float[n];
-    samples2 = new float[n];
+    short a, p;
+    float* samples1;
+    float* samples2;
     float dt = 1. / f / 20.0;
+    const float pi = 3.141592;
+    float rr[200];
+    float RR[200];
+    samples1 = new float[n];
+    samples2 = new float[n];
+
+    //기초값 설정
+    a = 30; p = 10;
+
 
     //assignment1
     for (int i = 0; i < n; i++) {
-        samples[i] = (float)a * sin(2 * pi * f * i * dt);
+        samples1[i] = (float)a * sin(2 * pi * f * i * dt);
     }
     ofstream ao("a.dat", ios::binary | ios::out);
     if (!ao) return -1;
-    ao.write((char*)samples, n * sizeof(float));
+    ao.write((char*)samples1, n * sizeof(float));
     ao.close();
 
     //assingment2
     ifstream ai("a.dat", ios::binary | ios::in);
     if (!ai) return -2;
-    ai.read((char*)rr, 12);
-    for (int i = 0; i < 3; i++) {
+    ai.read((char*)rr, sizeof(rr));
+    for (int i = 0; i < 200; i++) {
         cout << rr[i] << endl;
     }
-    ai.close();
-    
+   
+    float dt2 = dt;
     f = 200;
     for (int i = 0; i < n; i++) {
-        samples2[i] = (float)a * sin(2 * pi * f * i * dt);
+        samples2[i] = (float)a * sin(2 * pi * f * i * dt2);
     }
     ofstream bo("b.dat", ios::binary | ios::out);
     if (!bo) return -3;
-    ao.write((char*)samples2, n * sizeof(float));
-    ao.close();
-    
+    bo.write((char*)samples2, n * sizeof(float));
+    bo.close();
+
     //assingment3
     ifstream ai_2("a.dat", ios::binary | ios::in);
     if (!ai_2) return -4;
@@ -50,7 +56,7 @@ int main() {
     ifstream bi_2("b.dat", ios::binary | ios::in);
     if (!bi_2) return -5;
     bi_2.read((char*)RR, sizeof(RR));
-    
+
     ofstream co("c.txt");
     if (!co) return -6;
     for (int i = 0; i < n; i++) {
@@ -58,6 +64,6 @@ int main() {
     }
     co.close();
     return 123;
-    
+
 }
 
